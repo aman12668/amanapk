1efa5590ed9b22e4808361423b77ac00117ae9c2
aman sapkota <fnode.manage.app@gmail.com>
2023-11-20 12:11:02 +0545

    initial commit

--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "D:\\flutter_windows_3.13.2-stable\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "D:\\aman-project11" PROJECT_DIR)
+file(TO_CMAKE_PATH "D:\\NEB resource\\aman-project11" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -11,14 +11,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=D:\\flutter_windows_3.13.2-stable\\flutter"
-  "PROJECT_DIR=D:\\aman-project11"
+  "PROJECT_DIR=D:\\NEB resource\\aman-project11"
   "FLUTTER_ROOT=D:\\flutter_windows_3.13.2-stable\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=D:\\aman-project11\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=D:\\aman-project11"
-  "FLUTTER_TARGET=D:\\aman-project11\\lib\\main.dart"
+  "FLUTTER_EPHEMERAL_DIR=D:\\NEB resource\\aman-project11\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\NEB resource\\aman-project11"
+  "FLUTTER_TARGET=D:\\NEB resource\\aman-project11\\lib\\main.dart"
   "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iMjAxODNlMDQwOTYwOTRiY2MzN2Q5Y2RlMmE0Yjk2ZjVjYzY4NGNmLw=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=D:\\aman-project11\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=D:\\NEB resource\\aman-project11\\.dart_tool\\package_config.json"
 )
